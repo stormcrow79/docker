f96dbf99e5e7c8721c94a004886813fc7eec255b
Gavin Morris <gavin@almostpurple.com>
2018-12-25 15:32:29 +0800

    fix volume command by quoting the slash (?)

--- firebird/fb25.sh
+++ firebird/fb25.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker run -d -p 3050:3050 --name fb25 \
+  --mount "src=firebird,dst=//firebird" \
   -e "ISC_PASSWORD=password" \
   -e "FIREBIRD_DATABASE=foo" \
   -e "FIREBIRD_USER=bar" \
   -e "FIREBIRD_PASSWORD=baz" \
   jacobalberty/firebird:2.5-ss
 
-#  --mount "type=volume,src=firebird,dst=/firebird" \
